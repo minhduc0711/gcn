348799db61bdf7d1fa77af4f32c5cff9ac057872
minhduc0711 <phamminhduc0711@gmail.com>
2021-01-20 10:46:00 +0700

    small change to GAT

--- src/models/gat.py
+++ src/models/gat.py
@@ -11,7 +11,8 @@ from .base import BaseGNN
 class GAT(BaseGNN):
     def __init__(self, in_feats, hidden_feats, num_classes, num_hidden_layers,
                  num_heads=3,
-                 attn_drop=0.5,
+                 feat_drop=0.0,
+                 attn_drop=0.0,
                  **kwargs):
         super().__init__(**kwargs)
         self.save_hyperparameters()
@@ -20,20 +21,28 @@ class GAT(BaseGNN):
         last_input_dim = in_feats
         for i in range(num_hidden_layers):
             self.layers.append(GATConv(
-                in_feats=last_input_dim, out_feats=hidden_feats,
+                in_feats=last_input_dim,
+                out_feats=hidden_feats,
                 num_heads=num_heads,
+                feat_drop=feat_drop,
                 attn_drop=attn_drop
             ))
-            last_input_dim = hidden_feats
+            last_input_dim = hidden_feats * num_heads
         self.layers.append(GATConv(
             in_feats=last_input_dim, out_feats=num_classes,
             num_heads=num_heads,
+            feat_drop=feat_drop,
             attn_drop=attn_drop
         ))
 
     def forward(self, g, x):
-        for layer in self.layers:
+        batch_size = x.shape[0]
+        for i, layer in enumerate(self.layers):
             x = layer(g, x)
-            # average over all attention heads
-            x = x.mean(axis=1)
+            if i != len(self.layers) - 1:
+                # concatenate attention heads at hidden layers
+                x = x.reshape(batch_size, -1)
+            else:
+                # average over all attention heads at output layer
+                x = x.mean(axis=1)
         return x
